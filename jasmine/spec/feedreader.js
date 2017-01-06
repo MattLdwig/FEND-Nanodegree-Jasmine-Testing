4fbd1781d88d8e02dfd86d04099c2e6da28abe04
Wapika <mattldwig@gmail.com>
2017-01-06 11:00:02 +0100

    Refactoring JS, supp extra lines

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -12,7 +12,6 @@ $(function() {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
-
     /* Loop through each feed in the allFeeds
      *  and checks if it has an URL defined and not empty
      */
@@ -22,7 +21,6 @@ $(function() {
         expect(feed.url).not.toBe(0);
       });
     });
-
     /* Loop through each feed in the allFeeds
      *  and checks if it has an name defined and not empty
      */
@@ -33,19 +31,16 @@ $(function() {
       });
     });
   });
-
   // Second test suite. Sidebar Menu 
   describe('The menu', function() {
     var body = document.body;
     var menuIcon = document.querySelector('.menu-icon-link');
-
     /* Check if the body has the class .menu-hidden,
      *  meaning that the menu is hidden.
      */
     it('is hidden by default', function() {
       expect(body.className).toBe('menu-hidden');
     });;
-
     /* Check if the body doesn't have the class .menu-hidden
      *  after a click on the menu icon. Then check if the class
      *  .menu-hidden reappears after a second click on the menu icon.
@@ -53,12 +48,10 @@ $(function() {
     it('toggle visibility when the menu icon is clicked', function() {
       menuIcon.click();
       expect(body.className).not.toBe('menu-hidden');
-
       menuIcon.click();
       expect(body.className).toBe('menu-hidden');
     });
   });
-
   // Third test suite. Initial Entries.
   describe('Initial Entries', function() {
     // Call a function to do an async request
@@ -67,7 +60,6 @@ $(function() {
         done();
       });
     });
-
     /* Check if the loadFeed function has at least
      *  one .entry element within the .feed container.
      *  
@@ -75,33 +67,25 @@ $(function() {
     it('has at least an entry after loading', function(done) {
       var feed = document.querySelector('.feed'),
         feedEntries = feed.getElementsByClassName('entry');
-
       expect(feedEntries.length).toBeGreaterThan(0);
       done();
     });
-
   });
-
   // Fourth test suite. News Feeds Loaded.
   describe('New Feed Selection', function() {
     var feedBefore,
       feedAfter;
-
     /* Call the feed and store the content of the .feed
      *  container in a variable.
      */
     beforeEach(function(done) {
       loadFeed(0, function() {
-
         feedBefore = document.querySelector('.feed').innerHTML;
-
         loadFeed(1, function() {
           done();
         });
-
       });
     });
-
     /* Compares the first and the second feed content and
      *  check if the content is different.
      */
@@ -109,7 +93,5 @@ $(function() {
       feedAfter = document.querySelector('.feed').innerHTML;
       expect(feedAfter).not.toEqual(feedBefore);
     });
-
   });
-
 }());
